c7744a8e91380a71c86c712fdc99228872e9843e
Derrick Bommarito <derrick.a.bommarito@gmail.com>
2017-01-22 16:01:08 -0600

    Added opencv and raspicam flags for easy integration in this build system

--- flags.mk
+++ flags.mk
@@ -8,3 +8,20 @@ LDFLAGS=
 ARFLAGS= rc
 
 CLEANFILES+=*.o *~ *.a
+
+#The pseudo targets below automate settings up the flags for various
+#packages. To use them, add your targets as dependencies to these targets.
+#IE for an executable that uses the raspicam shared libraries and opencv:
+#   opencv: cam_test
+#   raspicam: cam_test
+#
+#   cam_test: cam_test.o
+
+.PHONY: opencv
+opencv: CXXFLAGS += `pkg-config opencv --cflags`
+opencv: LDFLAGS += `pkg-config opencv --libs`
+
+.PHONY: raspicam
+raspicam: opencv
+raspicam: CXXFLAGS += -I/usr/local/include
+raspicam: LDFLAGS += -L/usr/local/lib -lraspicam_cv -lraspicam
